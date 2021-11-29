8240f04f17f4ea2623869f585bdc1fb4a2678541
Miguel Angel Zetina Zetina <loefthur26@gmail.com>
2021-11-29 17:10:14 -0600

    Add alembic for migrations

--- core/config.py
+++ core/config.py
@@ -10,7 +10,7 @@ class Settings:
     POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "db")
     POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", 5432)
     POSTGRES_DB: str = os.getenv("POSTGRES_DB", "postgres")
-    DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
     SECRET_KEY: str = os.getenv("SECRET_KEY")
     ALGORITHM = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES = 15
--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
 
 from apis.base import api_router
-from db.session import engine
-from db.base import Base
 from core.config import settings
-
-Base.metadata.create_all(bind=engine)
 
 
 def include_router(app):
@@ -14,7 +10,6 @@ def include_router(app):
 
 def start_application():
     app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
-    Base.metadata.create_all(bind=engine)
     include_router(app)
     return app
 
