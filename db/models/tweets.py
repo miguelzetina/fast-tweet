5aaadfa19338218fc31e1a0c13993b14b0fd64cf
Miguel Angel Zetina Zetina <loefthur26@gmail.com>
2021-11-29 17:26:19 -0600

    Add tweet migrations

--- db/models/tweets.py
+++ db/models/tweets.py
@@ -1,25 +1,20 @@
 import uuid
-from sqlalchemy import Boolean, Column, String, Date, DateTime, func
+from sqlalchemy import Boolean, Column, String, Date, DateTime, func, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 
 from db.base_class import Base
 
 
-class User(Base):
+class Tweet(Base):
     id = Column(
         UUID(as_uuid=True),
         primary_key=True,
         index=True,
         default=uuid.uuid4,
     )
-    email = Column(String, nullable=False, unique=True, index=True)
-    hashed_password = Column(String, nullable=False)
+    content = Column(String, nullable=False)
     is_active = Column(Boolean(), default=True)
-    is_superuser = Column(Boolean(), default=False)
-    first_name = Column(String(256), nullable=False)
-    last_name = Column(String(256), nullable=False)
-    birth_date = Column(Date, nullable=True)
     created_at = Column(
         DateTime,
         nullable=False,
@@ -31,6 +26,7 @@ class User(Base):
         server_default=func.now(),
         server_onupdate=func.now(),
     )
-    tweets = relationship(
-        "Tweet", foreign_keys="[Tweet.user_id]", back_populates="user"
+    user_id = Column(UUID(as_uuid=True), ForeignKey("user.id"), nullable=False)
+    user = relationship(
+        "User", foreign_keys=[user_id], back_populates="tweets"
     )
